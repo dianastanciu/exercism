34ead8614abc22a2f638f61b2e41232cc8c9b579
dianastanciu <dianastanciu13@yahoo.co.uk>
2018-11-19 13:07:58 +0200

    update leap after feedback

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -1,22 +1,14 @@
 export const isLeap = (num) => {
     if(num % 4 === 0) {
-
-        if(num % 100 !== 0) {
+        if(num % 100 === 0) {
+            if(num % 400 === 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
             return true;
-        } else if (num % 100 === 0 && num % 400 !== 0) {
-            return false;
         }
-
-        if(num % 400 === 0) {
-            return true;
-        }
-
-        if (num % 200 === 0 && num % 400 !== 0) {
-            return false;
-        }
-
-        return true;
-
     } else {
         return false;
     }
