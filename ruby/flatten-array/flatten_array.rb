38c6274824a4851b7c500dd89ad5b3fba4dececf
dianastanciu <dianastanciu13@yahoo.co.uk>
2019-01-17 15:23:37 +0200

    rb flatten solution

--- ruby/flatten-array/flatten_array.rb
+++ ruby/flatten-array/flatten_array.rb
@@ -1,15 +1,15 @@
 class FlattenArray
-    def self.flatten(items)
-        items_list = []
-
-        items.each do |item|
-            if item.class == Array
-                item.each { |value| items_list << value }
-            else
-                items_list << item
+    def self.flatten(items, items_results = [])
+        items.each do |value|
+            if !value.nil?
+                if value.class == Array
+                    flatten(value, items_results)
+                else
+                    items_results << value
+                end
             end
         end
         
-        items_list
+        items_results
     end
 end
