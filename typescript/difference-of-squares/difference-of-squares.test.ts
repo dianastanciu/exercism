51781738e205942735ba2209430cc82397aad4e8
dianastanciu <dianastanciu13@yahoo.co.uk>
2019-01-18 13:54:27 +0200

    Update difference-of-squares.test.ts

--- typescript/difference-of-squares/difference-of-squares.test.ts
+++ typescript/difference-of-squares/difference-of-squares.test.ts
@@ -6,15 +6,14 @@ describe('Squares', () => {
     const squares = new Squares(5)
 
     it('gets the square of sum', () => {
-      expect(squares.squareOfSum).toBe(225) //DONE
+      expect(squares.squareOfSum).toBe(225)
     })
 
     xit('gets the sum of squares', () => {
-      expect(squares.sumOfSquares).toBe(55) //DONE
-    })
+      expect(squares.sumOfSquares).toBe(55)
 
     xit('gets the difference', () => {
-      expect(squares.difference).toBe(170) //DONE
+      expect(squares.difference).toBe(170)
     })
 
   })
@@ -23,15 +22,14 @@ describe('Squares', () => {
     const squares = new Squares(10)
 
     xit('gets the square of sum', () => {
-      expect(squares.squareOfSum).toBe(3025) //DONE
-    })
+      expect(squares.squareOfSum).toBe(3025)
 
     xit('gets the sum of squares', () => {
-      expect(squares.sumOfSquares).toBe(385) //DONE
+      expect(squares.sumOfSquares).toBe(385)
     })
 
     xit('gets the difference', () => {
-      expect(squares.difference).toBe(2640) //DONE
+      expect(squares.difference).toBe(2640)
     })
 
   })
@@ -40,15 +38,15 @@ describe('Squares', () => {
     const squares = new Squares(100)
 
     xit('gets the square of sum', () => {
-      expect(squares.squareOfSum).toBe(25502500) //DONE
+      expect(squares.squareOfSum).toBe(25502500)
     })
 
     xit('gets the sum of squares', () => {
-      expect(squares.sumOfSquares).toBe(338350) //DONE
+      expect(squares.sumOfSquares).toBe(338350)
     })
 
     xit('gets the difference', () => {
-      expect(squares.difference).toBe(25164150) //DONE
+      expect(squares.difference).toBe(25164150)
     })
 
   })
