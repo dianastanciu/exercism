2f5e8b7bb08d5e8893d14a7ee2330cc85e5516a3
dianastanciu <dianastanciu13@yahoo.co.uk>
2019-01-22 11:32:43 +0200

    Update acronym.rb

--- ruby/acronym/acronym.rb
+++ ruby/acronym/acronym.rb
@@ -1,22 +1,12 @@
 class Acronym
-  def self.abbreviate(string)
-    case string
-      when 'Portable Network Graphics'
-        'PNG'
-      when 'Ruby on Rails'
-        'ROR'
-      when 'First In, First Out'
-        'FIFO'
-      when 'GNU Image Manipulation Program'
-        'GIMP'
-      when 'Complementary metal-oxide semiconductor'
-        'CMOS'
-      when 'Rolling On The Floor Laughing So Hard That My Dogs Came Over And Licked Me'
-        'ROTFLSHTMDCOALM'
-      when 'Something - I made up from thin air'
-        'SIMUFTA'
-      else
-        'Sorry, I do not know this abbreviation'
-    end
+  def self.abbreviate(str)
+    words = str.split.map(&:capitalize)
+    firstChar = words.map { | i | 
+      i.slice(0)
+    }
+    abbreviation = firstChar.flatten
+    return abbreviation.join('')
   end
 end
+
+puts Acronym.abbreviate('Complementary metal-oxide semiconductor')
