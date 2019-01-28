17444faedd6b1db8dedfc788e43657e3d43ef533
dianastanciu <dianastanciu13@yahoo.co.uk>
2019-01-28 10:02:57 +0200

    js gigasecond init

---
+++ javascript/gigasecond/gigasecond.js
@@ -0,0 +1,9 @@
+// export default 
+export default function gigasecond(initialDate) {
+  this.initialDate = initialDate;
+  this.gigasecond = Math.pow(10, 9);
+};
+
+gigasecond.prototype.date = () => {
+  return new Date(Number(this.initialDate) + this.gigasecond * 1000)
+}
