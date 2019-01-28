551ac68a904c88914432f6c283af4ba383b74e14
dianastanciu <dianastanciu13@yahoo.co.uk>
2019-01-28 10:48:28 +0200

    rb acronym initial solution

--- ruby/acronym/acronym.rb
+++ ruby/acronym/acronym.rb
@@ -1,12 +1,22 @@
 class Acronym
   def self.abbreviate(str)
-    words = str.split.map(&:capitalize)
-    firstChar = words.map { | i | 
-      i.slice(0)
+    words = str.gsub(/(?<=[a-z])(?=-)/, ' ').gsub('-', '').gsub(',', '')
+    
+    list = words.split.map(&:capitalize)
+
+    firstChar = list.map { | i | 
+      if self.check_for_special_char(i)
+        ''
+      else
+        i.slice(0)
+      end
     }
-    abbreviation = firstChar.flatten
+
+    abbreviation = firstChar
     return abbreviation.join('')
   end
-end
 
-puts Acronym.abbreviate('Complementary metal-oxide semiconductor')
+  def self.check_for_special_char(string) 
+    /\W/ === string
+  end
+end
